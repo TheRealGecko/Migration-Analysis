6966cffc1633ca94d9d8e9cc8495acd8267f797e
Fatma Jadoon <fatma.jadoon7@gmail.com>
2025-07-08 02:39:25 -0400

    edited most_movement image

--- most_movement.py
+++ most_movement.py
@@ -41,6 +41,6 @@ avg_goose_dist = monthly_goose_dist.groupby("month")["total_month_dist"].mean().
 avg_goose_dist = avg_goose_dist.sort_values("month")
 avg_goose_dist["month_formatted"] = avg_goose_dist["month"].dt.strftime("%Y-%m")
 fig = px.bar(avg_goose_dist, x='month_formatted', y='avg_month_dist', labels={"month_formatted":"Month", "avg_month_dist":"Average Distance (km)"})
-fig.update_layout(margin={"r": 20, "t": 50, "l": 20, "b": 10}, title="Monthly Distance Travelled by North-East American Canada Geese 2017-2021", )
-fig.update_xaxes(tickangle = 90)
+fig.update_layout(margin={"r": 20, "t": 50, "l": 20, "b": 10}, title="Monthly Distance Travelled by NE American Canada Geese 2017-2021", )
+fig.update_xaxes(ticks='outside', ticklen=10, ticklabelindex=-1, tickangle=45)
 fig.write_image("results\\movement_per_month.png") 
